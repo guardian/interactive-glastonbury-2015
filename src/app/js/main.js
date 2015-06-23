ec1542f6c338b20f76b66331e4b094ab9b08c3f6
feilding <feilding.cage@gmail.com>
2015-06-23 11:28:27 +0100

    new format w mustache only

--- src/app/js/main.js
+++ src/app/js/main.js
@@ -1,39 +1,83 @@
-var Ractive = require('ractive');
+var Mustache = require('mustache');
+var Swiper = require('swiper');
 var getJSON = require('./utils/getjson'); 
-var app;
 
+var app;
+var el;
+var storyData;
+var windowSize;
 /**
  * Update app using fetched JSON data
  * @param {object:json} data - JSON spreedsheet data.
  */
-function updateView(data) {
-	app = new Ractive( {
-	    el: el,
-	    template: require('./templates/base.html'),
-	    data: {
-	    	stories: data.stories,
-	    	windowWidth: window.innerWidth,
-	    	windowHeight: window.innerHeight
-	    },
-		components: {
-			gallery: require('./components/gallery'),
-			stories: require('./components/stories'),
-			socialButtons: require('./components/socialButtons'),
-			slide: require('./components/slide')
-		}
-	});
-
-
-	window.addEventListener('onresize', measure);
+function render(data){
+	storyData = data;
+	updateView(storyData);
 }
+
+
+function updateView(data) {
+
+	var rendered = Mustache.render(
+						require('./templates/base.html'), 
+						{
+							stories: data.stories,
+							windowSize: windowSize,
+							shellLayout: (windowSize.windowWidth <=640) ? 'v' : 'h',
+							storyLayout: (windowSize.windowWidth <=640) ? 'h' : 'v'
+						},
+						{
+							gallery: require('./templates/gallery.html'),
+							slide: require('./templates/slide.html')
+						}
+					);
+	el.innerHTML = rendered;
+
+	//init horizontal sqipers
+	var vSwipers = el.getElementsByClassName('swiper-container-v')
+	var hSwipers = el.getElementsByClassName('swiper-container-h')
+
+	initSwipers(hSwipers, 'horizontal');
+	initSwipers(vSwipers, 'vertical');
+
+
+
+	
+
+	//window.addEventListener('onresize', measure);
+}
+
+function initSwipers(elems, direction){
+
+	for(var i = 0; i < elems.length; i++) {
+
+		new Swiper(elems[i], {
+	        pagination: elems[i].getElementsByClassName('swiper-pagination-' + direction.charAt(0) )[0] ,
+	        paginationClickable: true,
+	        spaceBetween: 1,
+	        direction: direction,
+	        paginationClickable: elems[i].getElementsByClassName('swiper-pagination-' + direction.charAt(0) )[0],
+	        nextButton: elems[i].getElementsByClassName('swiper-button-next'),
+	        prevButton: elems[i].getElementsByClassName('swiper-button-prev'),
+	        keyboardControl: true,
+	        mousewheelControl: (direction === 'vertical') ? true : false
+	    });
+
+	}
+
+}
+
+
 
 
 /**
  * Boot the app.
  * @param {object:dom} el - <figure> element passed by boot.js. 
  */
-function boot(el) {
+function boot(div) {
 
+	el = div;
+	windowSize = measure();
 	
 	var key = '1o-i8CBAkcbm1t-qNKECzh6VKhlhqygPh2dBhSf-ygLQ';
 	var folder = (window.location.hostname.search('localhost') > -1 ) ? 'docsdata-test' : 'docsdata';
@@ -42,12 +86,12 @@ function boot(el) {
 }
 
 function measure(){
-	var params = {
+	return params = {
 		windowWidth: window.innerWidth,
 		windowHeight: window.innerHeight
 	};
 
-	app.set(params);
+
 }
 
 // AMD define for boot.js
